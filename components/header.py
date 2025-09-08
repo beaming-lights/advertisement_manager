50c7a87e85b24875103e4b77f8ec37488b3bce29
lazy.logic <abstudios220@gmail.com>
2025-09-08 17:02:06 +0100

    fix navigation

--- components/header.py
+++ components/header.py
@@ -3,9 +3,8 @@ from pages.home import show_home_page as home_page
 
 def show_header():
     with ui.row():
-        ui.link('Advertisement Manager', '/home')
-        ui.link('add event', '/add_event')
-        ui.button('Home', on_click=home_page)
-        ui.button('Add Event', on_click=lambda: app.open('/add_event'))
-        ui.button('View Event', on_click=lambda: app.open('/view_event'))
-        ui.button('Edit Event', on_click=lambda: app.open('/edit_event'))
+        ui.button('Home', on_click=lambda: ui.open('/home'))
+        ui.button('Add Event', on_click=lambda: ui.open('/add_event'))
+        ui.button('View Event', on_click=lambda: ui.open('/view_event'))
+        ui.button('Edit Event', on_click=lambda: ui.open('/edit_event'))
+        ui.button('Log Out', on_click=lambda: ui.open('/'))
